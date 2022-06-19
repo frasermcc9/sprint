49a47eb309fb6142e2ee7e0acd92bd137e0661a5
Fraser McCallum <fraser@themccallums.net.nz>
2022-06-19 18:31:04 +1200

    Improve docs/usage

--- tools/seed/setup-seed.ts
+++ tools/seed/setup-seed.ts
@@ -3,12 +3,14 @@ import { green, red } from "colors";
 import { mkdir, rmdir } from "fs/promises";
 import { execSync } from "child_process";
 
+const PROMPT_MESSAGE =
+  "Are you sure you want to create a new seed for the database? The current database state will be used in future seeding. This information IS committed to the repo, so ensure there are no sensitive information in the database.";
+
 (async () => {
   const { confirmed } = await prompts({
     type: "confirm",
     name: "confirmed",
-    message:
-      "Are you sure you want to create a new seed for the database? The current database state will be used in future seeding.",
+    message: PROMPT_MESSAGE,
   });
 
   if (!confirmed) {
