ad988170c5782e2e8d754d90134b9e475f23bdc2
Bruce Zeng <bruce.zeng@xtra.co.nz>
2022-06-29 16:59:27 +1200

    setup intraday hr fetch

--- apps/backend/src/app/controllers/run/run.resolver.ts
+++ apps/backend/src/app/controllers/run/run.resolver.ts
@@ -1,8 +1,17 @@
-import { Resolver } from "@nestjs/graphql";
+import { Resolver, Query, Mutation, Args } from "@nestjs/graphql";
 // import { Run as GQLRun } from "../../types/graphql";
 import { RunService } from "./run.service";
 
 @Resolver("Run")
 export class RunResolver {
   constructor(private readonly runService: RunService) {}
+
+  @Mutation()
+  async createRun(
+    @Args("token") token: string,
+    @Args("startDate") startDate: string,
+    @Args("endDate") endDate: string,
+  ) {
+    return this.runService.createRun(token, startDate, endDate);
+  }
 }
--- apps/backend/src/app/controllers/run/run.service.ts
+++ apps/backend/src/app/controllers/run/run.service.ts
@@ -1,4 +1,4 @@
-import { Injectable } from "@nestjs/common";
+import { Injectable, UnauthorizedException } from "@nestjs/common";
 import { InjectModel } from "@nestjs/mongoose";
 import { Run, RunCollection } from "../../db/schema/run.schema";
 
@@ -10,5 +10,31 @@ export class RunService {
 
   getRun(userId: string) {
     return this.runModel.findOne({ userId });
+  }
+
+  /**
+   * Fetch activity heart rate series from Fitbit API
+   * @param token Fitbit access token
+   * @param dateStart date in format YYYY-MM-DD
+   * @param dateEnd date in format YYYY-MM-DD
+   */
+  async createRun(access_token: string, dateStart: string, dateEnd: string) {
+    try {
+      fetch(
+        "https://api.fitbit.com/1/user/-/activities/heart/date/" +
+          dateStart +
+          "/" +
+          dateEnd +
+          "/1min.json",
+        {
+          method: "GET",
+          headers: { Authorization: `Bearer ${access_token}` },
+        },
+      )
+        .then((res) => res.json())
+        .then((json) => console.log(json));
+    } catch (e) {
+      console.error(e.toJSON());
+    }
   }
 }
